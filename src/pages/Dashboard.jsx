fd568aa4be0401a491fb78c7b4148416ea6d534d
Azhar Zisan <azharzisanxtr@gmail.com>
2026-04-08 21:35:59 +0100

    ui fixes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,8 +5,8 @@ import CircleChart from "../components/CircleChart";
 const Dashboard = () => {
   return (
     <>
-      <div className="w-full p-4 min-h-screen">
-        <div className="w-auto h-auto text-center">
+      <div className="w-full p-4 h-auto">
+        <div className="w-full text-center">
           <h2 className="text-2xl font-bold text-(--primary) p-4">Dashboard</h2>
         </div>
 
@@ -14,11 +14,13 @@ const Dashboard = () => {
           <div className="border-2 col-span-2 border-[#3a5a40] rounded-2xl">
             <BarChart />
           </div>
-          <div className="w-full h-auto border-2 row-span-2 row-start-2">
+          <div className="w-full h-auto row-span-2 row-start-2 relative">
             <CircleChart />
-            <p className="text-xl text-(--primary)">65%</p>
+            <p className="text-3xl text-(--primary) absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+              65%
+            </p>
           </div>
-          <div className="border-2 row-start-2">5</div>
+          <div className="w-full h-auto border-2 row-start-2">hello</div>
           <div className="border-2 col-start-2 row-start-3">6</div>
         </div>
       </div>
