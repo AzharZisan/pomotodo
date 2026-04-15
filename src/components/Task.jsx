abdaa0e4ebda6d6e6b28fa5c38230b5a801f9066
Azhar Zisan <azharzisanxtr@gmail.com>
2026-04-15 21:06:09 +0100

    checked sign is stable and work according to localstorage data

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
 import { MdDeleteOutline } from "react-icons/md";
 
-const Task = ({taskValue, priorityValue, taskId}) => {
-  const [onChecked, setOnChecked] = useState(false)
-  const taskData = JSON.parse(localStorage.getItem('tasklist')) || []
+const Task = ({ taskValue, priorityValue, taskId }) => {
+  const taskData = JSON.parse(localStorage.getItem("tasklist")) || [];
+  const [onChecked, setOnChecked] = useState(() => {
+    const target = taskData
+      .flatMap((i) => i.entries)
+      .find((e) => e.id === taskId);
+    return target?.checked ?? false;
+  });
 
   const handleOnChecked = () => {
-    const newChecked = !onChecked
-    setOnChecked(newChecked)
-    
+    const newChecked = !onChecked;
+    setOnChecked(newChecked);
+
     const updated = taskData.map((e) => ({
       ...e,
-      entries: e.entries.map((i) => 
-      i.id === taskId ? {...i, checked: newChecked} : i)
-    }))
-    localStorage.setItem('tasklist', JSON.stringify(updated))
-  }
-  
+      entries: e.entries.map((i) =>
+        i.id === taskId ? { ...i, checked: newChecked } : i,
+      ),
+    }));
+    localStorage.setItem("tasklist", JSON.stringify(updated));
+  };
+
   return (
     <>
       <div className="w-full h-auto flex justify-between items-start py-2 px-3 my-3 bg-(--bg-lite) rounded-xl">
@@ -25,10 +31,13 @@ const Task = ({taskValue, priorityValue, taskId}) => {
             type="checkbox"
             name=""
             id=""
+            checked={onChecked}
             onClick={handleOnChecked}
             className="translate-y-2"
           />
-          <p className={`text-lg flex ${onChecked ? `line-through` : ``}`}>
+          <p
+            className={`text-lg flex ${onChecked === true ? `line-through` : ``}`}
+          >
             {taskValue}
           </p>
         </div>
