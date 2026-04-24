b89f65db6a2f16665a399b9359c80fda7301fe45
Azhar Zisan <azharzisanxtr@gmail.com>
2026-04-24 19:39:28 +0100

    labels and dataset prop added from dashbaord and labels fot tooltip is hidden for better time parse

--- src/components/CircleChart.jsx
+++ src/components/CircleChart.jsx
@@ -9,6 +9,7 @@ import {
   Title,
   Tooltip,
   Legend,
+  scales,
 } from "chart.js";
 
 ChartJS.register(
@@ -20,23 +21,34 @@ ChartJS.register(
   Tooltip,
   Legend,
 );
-const CircleChart = () => {
+const CircleChart = ({CirLabels, CirDataValues}) => {
     const data = {
-      labels: ["A"],
+      labels: CirLabels,
       datasets: [
         {
-          data: [65],
+          data: CirDataValues,
           backgroundColor: ["rgba(88, 129, 87, 0.5)"],
           borderWidth: 1,
           borderColor: ["rgba(88, 129, 87)"],
           label: "",
         },
       ],
-    }
+    };
     const options = {
       plugins: {
         legend: {
           display: false,
+        },
+        tooltip: {
+          backgroundColor: "rgb(52, 78, 65)",
+          titleColor: "#dad7cd",
+          titleAlign: "center",
+          padding: 10,
+          callbacks: {
+            label: () => null,
+          },
+          displayColors: false,
+          bodyAlign: "center",
         },
       },
     };
